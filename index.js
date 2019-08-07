245f9b2d89bc17eb2a9d57f9d2a6b665c2417b60
micaelbarreto <micaelplayer98@gmail.com>
2019-08-07 09:02:17 -0300

    Routing

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => {
-    res.status(200).send('My Backend!')
+app.use(bodyParser.json())
+
+app.get('/teste/:valor', (req, res, next) => {
+    //res.status(200).send('My Backend! ')
+    next()
+})
+
+app.get('/teste/:valor', (req, res, next) => {
+    res.status(200).send('My Backend! =' + req)
+    //next()
+})
+
+app.post('/teste/post', (req, res, next) => {
+    res.status(200).send('My Backend! =' + req.body.name)
+    //next()
 })
 
 app.listen(3000, () => {
